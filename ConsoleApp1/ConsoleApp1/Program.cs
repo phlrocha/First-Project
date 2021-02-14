0a55817599c85504bec1ec0fde04690bf4b7bed0
Pedro Henrique Lima da Rocha <phrocha1@outlook.com>
2021-02-14 19:33:29 -0300

    changed to default

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("teste");
+            Console.WriteLine("Hello World");
         }
     }
 }
